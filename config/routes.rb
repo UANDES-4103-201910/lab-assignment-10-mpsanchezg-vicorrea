ca1caf4130ca890ced9c7c2e54c061a226cb84f1
Vicentecorrea <vicorrea@miuandes.cl>
2019-05-23 00:33:21 -0400

    Added event views and more checking abilities

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
@@ -10,9 +11,5 @@ Rails.application.routes.draw do
 
   get '/sign_in' => 'registrations#new', as: :registrations
   post '/sign_in' => 'registrations#create', as: :sign_in
-
-  # get 'users/:id', as: :profile
-
-  # get '/log_out', to: "sessions#new"
 
 end
