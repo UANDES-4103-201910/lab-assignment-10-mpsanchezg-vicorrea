c7a67232417268c293c88a77a0fd34d025fd08b9
Vicentecorrea <vicorrea@miuandes.cl>
2019-05-24 13:24:50 -0400

    Fixed places html and added link in nav bar and details in events html

--- app/models/event.rb
+++ app/models/event.rb
@@ -19,9 +19,4 @@ class Event < ApplicationRecord
       errors.add(:place, "Can't have 2 events at the same place and time")
     end
   end
-
-  def in_which_place
-    place = Place.find(place_id)
-  end
-
 end
